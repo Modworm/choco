bfc485e6884de3ae784eaf06cef3a75b5dd12e77
manav <manavjotsingh17@gmail.com>
2024-06-08 08:45:16 -0700

    added choco soil

---
+++ soil/tools/chocolateyuninstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = $env:ChocolateyPackageName
+$installDir = Join-Path $env:ChocolateyInstall 'bin'
+$exePath = Join-Path $installDir 'soil.exe'
+
+if (Test-Path $exePath) {
+    Write-Host "Removing $exePath"
+    Remove-Item $exePath -Force
+    Write-Host "$exePath has been removed"
+} else {
+    Write-Host "$exePath not found, nothing to remove"
+}
