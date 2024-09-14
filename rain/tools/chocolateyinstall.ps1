a05f39ae5c8b5777b5cbcad60e722f83a8664c3e
manav <manavjotsingh17@gmail.com>
2024-09-14 16:48:35 -0700

    updated rain install script

--- rain/tools/chocolateyinstall.ps1
+++ rain/tools/chocolateyinstall.ps1
@@ -1,30 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$tempDir = Join-Path $env:TEMP "chocolatey\$packageName"
 
 $zipUrl = 'https://modworm.com/rain_v1.0.0_windows.zip'
 $zipChecksum = '5c2b678604a7fa3fdcf36428f28d6dbe4a8813c5f9f4ea2f2d6385bc49f56e1e'
 $zipChecksumType = 'sha256'
 
-New-Item -ItemType Directory -Force -Path $tempDir | Out-Null
+$installDir = Join-Path $env:ChocolateyInstall 'lib' $packageName
 
-Write-Host "Downloading $zipUrl to $tempDir\$packageName.zip"
-$zipPath = Join-Path $tempDir "$packageName.zip"
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $zipPath -Url $zipUrl -Checksum $zipChecksum -ChecksumType $zipChecksumType
-
-Write-Host "Extracting $tempDir\$packageName.zip to $tempDir"
-Get-ChocolateyUnzip -FileFullPath $zipPath -Destination $tempDir
-
-
-$fileLocation = Join-Path $tempDir 'rain\rain.exe'
-
-$destination = Join-Path $env:ChocolateyInstall 'bin\rain.exe'
-
-Write-Host "Copying $fileLocation to $destination"
-Copy-Item -Path $fileLocation -Destination $destination -Force
-
-Write-Host "Cleaning up temporary files in $tempDir"
-Remove-Item -Path "$tempDir" -Recurse -Force
-
-Write-Host "$packageName has been installed to $destination"
+Install-ChocolateyZipPackage -PackageName $packageName -Url $zipUrl -UnzipLocation $installDir -Checksum $zipChecksum -ChecksumType $zipChecksumType
--- rain/tools/chocolateyuninstall.ps1
+++ rain/tools/chocolateyuninstall.ps1
@@ -1,13 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$installDir = Join-Path $env:ChocolateyInstall 'bin'
-$exePath = Join-Path $installDir 'rain.exe'
+$installDir = Join-Path $env:ChocolateyInstall 'lib' $packageName
 
-if (Test-Path $exePath) {
-    Write-Host "Removing $exePath"
-    Remove-Item $exePath -Force
-    Write-Host "$exePath has been removed"
+if (Test-Path $installDir) {
+    Write-Host "Removing $installDir"
+    Remove-Item -Force -Recurse $installDir
+    Write-Host "$installDir has been uninstalled."
 } else {
-    Write-Host "$exePath not found, nothing to remove"
+    Write-Host "$installDir not found, nothing to uninstall."
 }
