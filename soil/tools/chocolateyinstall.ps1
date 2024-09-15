0f6945dddd140675434aac4864164a381b7c0212
manav <manavjotsingh17@gmail.com>
2024-09-14 17:09:18 -0700

    added soil changes

--- soil/tools/chocolateyinstall.ps1
+++ soil/tools/chocolateyinstall.ps1
@@ -1,29 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$tempDir = Join-Path $env:TEMP "chocolatey\$packageName"
 
-$zipUrl = 'https://modworm.com/soil_v2.0.0_windows.zip'
-$zipChecksum = '5ff7076ecc2fd8775e9ec18fd9bd19edb46e76f300616ff9615ef508b8bfeaeb'
+$zipUrl = 'https://modworm.com/soil_v1.0.0_windows.zip'
+$zipChecksum = '42770dfdf73cf6d5fb166d73cde4cf0d8cba1b9f33c7a4b8fb33805b29cade1d'
 $zipChecksumType = 'sha256'  
 
-New-Item -ItemType Directory -Force -Path $tempDir | Out-Null
+$installDir = Join-Path $env:ChocolateyInstall 'lib' $packageName
 
-Write-Host "Downloading $zipUrl to $tempDir\$packageName.zip"
-$zipPath = Join-Path $tempDir "$packageName.zip"
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $zipPath -Url $zipUrl -Checksum $zipChecksum -ChecksumType $zipChecksumType
-
-Write-Host "Extracting $tempDir\$packageName.zip to $tempDir"
-Get-ChocolateyUnzip -FileFullPath $zipPath -Destination $tempDir
-
-$fileLocation = Join-Path $tempDir 'soil\soil.exe'
-
-$destination = Join-Path $env:ChocolateyInstall 'bin\soil.exe'
-
-Write-Host "Copying $fileLocation to $destination"
-Copy-Item -Path $fileLocation -Destination $destination -Force
-
-Write-Host "Cleaning up temporary files in $tempDir"
-Remove-Item -Path "$tempDir" -Recurse -Force
-
-Write-Host "$packageName has been installed to $destination"
+Install-ChocolateyZipPackage -PackageName $packageName -Url $zipUrl -UnzipLocation $installDir -Checksum $zipChecksum -ChecksumType $zipChecksumType
